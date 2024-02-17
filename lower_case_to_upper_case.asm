f1b60b57fc3b0e035909f20fc678ac3e7ac270d8
Mohaiminul Islam Nahid <125258523+nahid-stark@users.noreply.github.com>
2024-02-17 23:00:31 +0600

    Create lower_case_to_upper_case.asm

--- lower_case_to_upper_case.asm
+++ lower_case_to_upper_case.asm
@@ -4,24 +4,15 @@
 MAIN PROC
     MOV AH, 1
     INT 21H
-    
-    SUB AL, 20H 
-    
+    SUB AL, 20H
     MOV BL, AL
-    
     MOV AH, 2
-    MOV DL, ' '
-    INT 21H
-    
     MOV DL, 0AH
     INT 21H
-    MOV DL, 0DH
-    INT 21H  
-    
-    MOV AH, 2
+    MOV DL, 0DH 
+    INT 21H
     MOV DL, BL
     INT 21H
-    
     MOV AH, 4CH
     INT 21H
 MAIN ENDP
