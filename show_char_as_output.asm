3d2a16afb6f3b35fe51e2fd29d307e73189227ba
Mohaiminul Islam Nahid <125258523+nahid-stark@users.noreply.github.com>
2024-02-17 22:54:53 +0600

    Create show_char_as_output.asm

--- show_char_as_output.asm
+++ show_char_as_output.asm
@@ -1,29 +1,16 @@
 .MODEL SMALL
 .STACK 100H
-.DATA
-
 .CODE
 MAIN PROC
     MOV AH, 1
-    MOV DL, 'a'
     INT 21H
-    
     MOV BL, AL
-    
     MOV AH, 2
-    MOV DL, 'b'
-    INT 21H
-    
     MOV DL, 0AH
     INT 21H
-    MOV DL, 0DH
+    MOV DL, 0DH 
     INT 21H
-    
-    MOV AH, 2
     MOV DL, BL
-    INT 21H
-    
-    MOV AH, 4CH
     INT 21H
 MAIN ENDP
     END MAIN
