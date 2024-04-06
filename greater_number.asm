282f8e0edfd90e00704b2b0e4251db2a2a3955f3
Mohaiminul Islam Nahid <125258523+nahid-stark@users.noreply.github.com>
2024-04-06 18:19:57 +0600

    Create greater_number.asm

---
+++ greater_number.asm
@@ -0,0 +1,51 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    lineOne DB 'Enter First Number: ', '$'
+    lineTwo DB 'Enter Second Number: ', '$'
+    result DB 'Biggest one is: ', '$'
+.CODE
+MAIN PROC 
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET lineOne
+    INT 21H
+    MOV AH, 1
+    INT 21h
+    MOV BL, AL  
+    MOV AH, 2 
+    MOV DL, 0AH
+    INT 21H
+    MOV DL, 0DH 
+    INT 21H
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET lineTwo
+    INT 21H   
+    MOV AH, 1
+    INT 21H
+    MOV CL, AL    
+    MOV AH, 2    
+    MOV DL, 0AH
+    INT 21H
+    MOV DL, 0DH 
+    INT 21H
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET result
+    INT 21H   
+    CMP BL, CL
+    JG GREATER
+    MOV BL, CL
+    GREATER:
+    MOV BL, BL
+    MOV AH, 2
+    MOV DL, BL
+    INT 21h 
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+    END MAIN
