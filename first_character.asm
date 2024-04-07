3ef858020f204ad23434d70d261b18c0ca7e41da
Mohaiminul Islam Nahid <125258523+nahid-stark@users.noreply.github.com>
2024-04-07 13:56:31 +0600

    Create first_character.asm

---
+++ first_character.asm
@@ -0,0 +1,58 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    lineOne DB '(Input) Enter First Number: ', '$'
+    lineTwo DB '(Input) Enter Second Number: ', '$'
+    result DB '(Output) First one is: ', '$'
+.CODE
+MAIN PROC 
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET lineOne
+    INT 21H
+    MOV AH, 01h        
+    INT 21H
+    SUB AL, 30h        
+    MOV BL, AL         
+    MOV AH, 02h        
+    MOV DL, 0AH
+    INT 21H
+    MOV DL, 0DH 
+    INT 21H
+
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET lineTwo
+    INT 21H   
+    MOV AH, 01h        
+    INT 21H
+    SUB AL, 30h        
+    MOV CL, AL        
+    MOV AH, 02h       
+    MOV DL, 0AH
+    INT 21H
+    MOV DL, 0DH 
+    INT 21H
+
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 09h
+    MOV DX, OFFSET result
+    INT 21H   
+    CMP BL, CL        
+    JL LESSER        
+    MOV BL, CL         
+    JMP DONE           
+LESSER:
+    MOV BL, BL        
+DONE:
+    ADD BL, 30h        
+    MOV AH, 02h
+    MOV DL, BL
+    INT 21h 
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
